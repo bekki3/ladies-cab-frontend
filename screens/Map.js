2bb723ce91e1aae6fab9584bd27931a9b148d296
Bekhzod Shukhratov <bekhzod2204@gmail.com>
2023-07-19 21:45:52 +0500

    add get location to expo app

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { View, Button } from "react-native";
 import { WebView } from "react-native-webview";
-import { Asset } from "expo-asset";
-import { useAssets } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 import { readAsStringAsync } from 'expo-file-system';
 import { Linking } from "react-native";
+import * as Location from 'expo-location';
 
 
 const Map = () => {
     //const [location, setLocation] = useState(null);
+    const webViewRef = useRef(null);
     const [index, indexLoadingError] = useAssets(require("../assets/map.html"));
     const mapHtml = Asset.fromModule(require("../assets/map.html")).uri;
     const [html, setHtml] = useState("");
@@ -19,10 +20,20 @@ const Map = () => {
         });
     }
 
+    async function getLocationAsync() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      return location.coords;
+    }
+
     const deepLink = async (fromLatLng, toLatLng) => {
       try {
-        //Linking.openURL(`https://www.google.com/maps/dir/?api=1&origin=${fromLatLng.lat},${fromLatLng.lng}&destination=${toLatLng.lat},${toLatLng.lng}&travelmode=driving`);
-        Linking.openURL(`https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=${toLatLng.lat},${toLatLng.lng}&travelmode=driving`);
+        Linking.openURL(`https://www.google.com/maps/dir/?api=1&origin=${fromLatLng.lat},${fromLatLng.lng}&destination=${toLatLng.lat},${toLatLng.lng}&travelmode=driving`);
+        //Linking.openURL(`https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=${toLatLng.lat},${toLatLng.lng}&travelmode=driving`);
 
       } catch (error) {
         console.error(error);
@@ -42,6 +53,15 @@ const Map = () => {
             let fromLatLng = data.fromLatLng;
             let toLatLng = data.toLatLng;
             deepLink(fromLatLng, toLatLng);
+          }else if(data.command === "getLocation")
+          {
+            const promise = getLocationAsync();
+            promise.then((coords)=> {
+              const {latitude, longitude} = coords;
+              console.log("data", latitude, longitude);
+              webViewRef.current.injectJavaScript(`goToLocation(${latitude},${longitude})`); 
+            })
+                     
           }
         }
       };
@@ -50,6 +70,7 @@ const Map = () => {
         <View style={{ flex: 1 }}>
 
             <WebView
+                ref={webViewRef}
                 onLoad={() => {}}
                 source={{ html }}
                 onMessage={onMessage}
